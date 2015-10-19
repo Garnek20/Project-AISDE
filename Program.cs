a8e10d4cc32c808720073691b64e5d27532abcbc
Garnek <garnek522@gmail.com>
2015-10-19 23:02:27 +0200

    Second try

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolejka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+            Unsorted_list lista = new Unsorted_list();
+            lista.insert(a);
+
+
+
+        }
+    }
+}
