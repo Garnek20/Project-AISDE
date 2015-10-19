03f59f6f52de61f876297982dac9ade688458591
Garnek <garnek522@gmail.com>
2015-10-19 23:18:47 +0200

    Dodanie calosci?

---
+++ IKolejkaable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolejka
+{
+    public interface IKolejkaable
+    {
+        private List<Object object, > lista;
+        //metody kolejki
+        void insert(Object objekt);
+        void delete(Object objekt);
+        int dlugosc();
+    }
+}
---
+++ Unsorted-list.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolejka
+{
+    class Unsorted_list:IKolejkaable
+    {
+        int dlugosc()
+        {
+
+            return 
+        }
+        public void insert(Object objekt)
+        {
+            object lista = new object();
+            if(lista == "nullptr")
+
+        }
+
+        public void delete(Object objekt)
+        {
+
+        }
+    }
+
+}
